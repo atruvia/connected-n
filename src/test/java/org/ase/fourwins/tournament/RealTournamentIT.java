76ed895d136d5ebb8cdec3e97e96a927a9f08b91
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-11-12 21:03:41 +0100

    cleanup

--- src/test/java/org/ase/fourwins/tournament/RealTournamentIT.java
+++ src/test/java/org/ase/fourwins/tournament/RealTournamentIT.java
@@ -1,6 +1,5 @@
 package org.ase.fourwins.tournament;
 
-import static java.lang.String.format;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.toList;
 import static org.ase.fourwins.board.Board.Score.LOSE;
@@ -29,7 +28,7 @@ import net.jqwik.api.constraints.IntRange;
 
 public class RealTournamentIT {
 
-	private static final int MAX_PLAYERS = 20;
+	static final int MAX_PLAYERS = 20;
 
 	static final int MAX_SEASONS = 10;
 
@@ -40,7 +39,7 @@ public class RealTournamentIT {
 	void amountOfGames_2Players() {
 		List<PlayerMock> players = createPlayers(2, RandomMockPlayer::new);
 		int seasons = 1;
-		playSeasons(players, seasons);
+		playSeasons(seasons, players).forEach(this::verifyGameState);
 		verifyPlayers(players, seasons);
 	}
 
@@ -48,7 +47,7 @@ public class RealTournamentIT {
 	void amountOfGames_3Players() {
 		List<PlayerMock> players = createPlayers(3, RandomMockPlayer::new);
 		int seasons = 1;
-		playSeasons(players, seasons);
+		playSeasons(seasons, players).forEach(this::verifyGameState);
 		verifyPlayers(players, seasons);
 	}
 
@@ -56,7 +55,7 @@ public class RealTournamentIT {
 	void amountOfGames_4Players() {
 		List<PlayerMock> players = createPlayers(4, RandomMockPlayer::new);
 		int seasons = 1;
-		playSeasons(players, seasons);
+		playSeasons(seasons, players).forEach(this::verifyGameState);
 		verifyPlayers(players, seasons);
 	}
 
@@ -64,8 +63,7 @@ public class RealTournamentIT {
 	void jqwikCheckTestRandom(@ForAll @IntRange(min = 0, max = MAX_PLAYERS) int playerCount,
 			@ForAll @IntRange(min = 0, max = MAX_SEASONS) int seasons) {
 		List<PlayerMock> players = createPlayers(playerCount, RandomMockPlayer::new);
-		Tournament tournament = tournamentWithPlayers(seasons, players);
-		playSeasons(seasons, tournament).filter(isCoffeeBreak.negate()).forEach(this::verifyGameState);
+		playSeasons(seasons, players).forEach(this::verifyGameState);
 		verifyPlayers(players, seasons);
 	}
 
@@ -73,27 +71,24 @@ public class RealTournamentIT {
 	void jqwikCheckTestKeepTrack(@ForAll @IntRange(min = 0, max = MAX_PLAYERS) int playerCount,
 			@ForAll @IntRange(min = 0, max = MAX_SEASONS) int seasons) {
 		List<PlayerMock> players = createPlayers(playerCount, ColumnTrackingMockPlayer::new);
-		playSeasons(seasons, tournamentWithPlayers(seasons, players)).filter(isCoffeeBreak.negate())
-				.forEach(s -> assertThat(String.valueOf(s), s.getScore(), is(WIN)));
+		playSeasons(seasons, players).forEach(this::verifyGameState);
+		verifyPlayers(players, seasons);
 	}
 
 	List<PlayerMock> createPlayers(int players, Function<String, PlayerMock> function) {
 		return IntStream.range(0, players).mapToObj(String::valueOf).map("P"::concat).map(function).collect(toList());
 	}
 
-	void playSeasons(List<PlayerMock> players, int numberOfSeasons) {
-		Tournament tournament = tournamentWithPlayers(numberOfSeasons, players);
-		playSeasons(numberOfSeasons, tournament).filter(isCoffeeBreak.negate()).forEach(this::verifyGameState);
+	Stream<GameState> playSeasons(int seasons, List<PlayerMock> players) {
+		Tournament tournament = registerPlayers(new Tournament(), players);
+		return playSeasons(seasons, tournament).filter(isCoffeeBreak.negate());
 	}
 
 	Stream<GameState> playSeasons(int seasons, Tournament tournament) {
 		return IntStream.range(0, seasons).mapToObj(i -> tournament.playSeason()).flatMap(identity());
 	}
 
-	Tournament tournamentWithPlayers(int numberOfSeasons, Collection<PlayerMock> players) {
-		System.out
-				.println(format("Starting tournament with %s players and %s seasons", players.size(), numberOfSeasons));
-		Tournament tournament = new Tournament();
+	protected Tournament registerPlayers(Tournament tournament, Collection<PlayerMock> players) {
 		players.forEach(tournament::registerPlayer);
 		return tournament;
 	}
