14dbcd2d3f1debf0564a68870055d50b88b1d1ff
Frank Rehfeldt <Frank.Rehfeldt@fiduciagad.de>
2019-10-16 13:22:38 +0200

    pimp dashboard

--- docker/mysql/sql.sql
+++ docker/mysql/sql.sql
@@ -3,8 +3,13 @@ USE 4WINS;
 CREATE TABLE IF NOT EXISTS games(ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, player_id varchar(255), value double);
  
 insert into games (player_id, value) values
- ("A", 25.5),
- ("B", 30),
- ("C", 1),
- ("D", 0.5),
- ("E", 100);
+ ("Peter", 25.5),
+ ("Sebastian", 30),
+ ("Andy", 1),
+ ("Alex", 0.5),
+ ("Daniel", 100);
+
+insert into games (player_id, value) values
+ ("Peter", 26),
+ ("Andy", 2),
+ ("Alex", 82);
