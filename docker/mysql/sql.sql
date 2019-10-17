0fedc85e7410d70163b5cc653a19dd08a03eaa7d
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2019-10-17 14:20:18 +0200

    purged dummy data

--- docker/mysql/sql.sql
+++ docker/mysql/sql.sql
@@ -1,19 +1,4 @@
 CREATE DATABASE IF NOT EXISTS 4WINS;
-USE 4WINS;
-CREATE TABLE IF NOT EXISTS games(ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, player_id varchar(255), value double);
- 
-insert into games (player_id, value) values
- ("Peter", 25.5),
- ("Sebastian", 30),
- ("Andy", 1),
- ("Alex", 0.5),
- ("Daniel", 100);
-
-insert into games (player_id, value) values
- ("Peter", 26),
- ("Andy", 2),
- ("Alex", 82);
-
-create user fourwins_read identified by "fourwinsread";
-
-grant select on 4WINS.games to fourwins_read;
+CREATE TABLE IF NOT EXISTS games(ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, player_id VARCHAR(255), value DOUBLE);
+CREATE USER fourwins_read IDENTIFIED BY "fourwinsread";
+GRANT SELECT ON 4WINS.games TO fourwins_read;
