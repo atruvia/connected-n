f285af7dcb805ad74e0bd2102cf452e147621ee3
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2019-10-19 15:04:38 +0200

    house keeping

--- docker/mysql/sql.sql
+++ docker/mysql/sql.sql
@@ -2,20 +2,11 @@ CREATE DATABASE IF NOT EXISTS 4WINS;
 USE 4WINS;
 CREATE TABLE IF NOT EXISTS games(ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, player_id varchar(255), value double);
  
-insert into games (player_id, value) values
- ("Peter", 25.5),
- ("Sebastian", 30),
- ("Andy", 1),
- ("Alex", 0.5),
- ("Daniel", 100);
+CREATE USER IF NOT EXISTS 'fourwins_read'@'%' IDENTIFIED BY 'fourwinsread';
+CREATE USER IF NOT EXISTS 'fourwins_write'@'%' IDENTIFIED BY 'fourwinswrite';
 
-insert into games (player_id, value) values
- ("Peter", 26),
- ("Andy", 2),
- ("Alex", 82);
+GRANT SELECT ON 4WINS.* TO 'fourwins_read'@'%';
+GRANT ALL PRIVILEGES ON 4WINS.* TO 'fourwins_write'@'%';
 
-create user fourwins_read identified by "fourwinsread";
-create user fourwins_write identified by "fourwinswrite";
+FLUSH PRIVILEGES; 
 
-grant select on 4WINS.games to fourwins_read;
-grant all privileges on 4WINS.games to fourwins_write;
