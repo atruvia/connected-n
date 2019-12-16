1f8172820497904710d6e9b388339be82496dc1a
Oliver Erxleben <oliver.erxleben@me.com>
2019-12-16 22:08:07 +0100

    added pact dependencies

--- webapp/karma.conf.js
+++ webapp/karma.conf.js
@@ -27,6 +27,17 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    pact: [{
+      cors: true,
+      port: 1234,
+      consumer: "ui",
+      provider: "gameservice",
+      dir: "pacts",
+      spec: 2
+    }],
+    proxies: {
+      '/games/': 'http://127.0.0.1:1234/games/'
+    }
   });
 };
