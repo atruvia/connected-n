b2211e6dcf8be89a009fc0d54cb6c1fa29e0ec5d
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2019-12-22 17:52:02 +0100

    #8 fixed broken build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@
 DOCKER_IMAGE=maven:3.6-jdk-8
 PRJ_NAME=`basename "$PWD"`
 
-winpty docker run -it --rm \
+docker run -it --rm \
 -v "$HOME/.m2":/root/.m2 \
 -v "$PWD":/usr/src/$PRJ_NAME \
 -w /usr/src/$PRJ_NAME \
 $DOCKER_IMAGE \
 mvn package jib:buildTar && docker image load -i mysql/target/jib-image.tar
+
