027f564eba5c639b2af113860799f957f8ff028f
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2020-03-08 14:31:48 +0100

    cleanup

--- udp/src/test/java/org/ase/fourwins/udp/server/IgnoreExceptionDelegateTournamentListenerTest.java
+++ udp/src/test/java/org/ase/fourwins/udp/server/IgnoreExceptionDelegateTournamentListenerTest.java
@@ -2,27 +2,24 @@ package org.ase.fourwins.udp.server;
 
 import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemErr;
 import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toSet;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockingDetails;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import org.ase.fourwins.tournament.listener.TournamentListener;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
+import org.mockito.invocation.Invocation;
 
 class IgnoreExceptionDelegateTournamentListenerTest {
-
-	private final IgnoreExceptionDelegateTournamentListener sut = new IgnoreExceptionDelegateTournamentListener(
-			throwEx());
-
-	private static TournamentListener throwEx() {
-		return mock(TournamentListener.class, i -> {
-			throw new UnsupportedOperationException("method: " + i.getMethod());
-		});
-	}
 
 	@TestFactory
 	Stream<DynamicTest> allExceptionsToCallsToInterfaceMethodAreCaught() {
@@ -30,7 +27,19 @@ class IgnoreExceptionDelegateTournamentListenerTest {
 	}
 
 	private DynamicTest callMethodOnSut(Method method) {
-		return dynamicTest(method.getName(), () -> tapSystemErr(() -> method.invoke(sut, emptyArgs(method))));
+		TournamentListener mock = mock(TournamentListener.class, i -> {
+			throw new UnsupportedOperationException("method: " + i.getMethod());
+		});
+		TournamentListener sut = new IgnoreExceptionDelegateTournamentListener(mock);
+
+		return dynamicTest(method.getName(), () -> {
+			assertThat(tapSystemErr(() -> method.invoke(sut, emptyArgs(method))).isEmpty(), is(false));
+			assertThat(Set.of(method), is(methodsCalled(mock)));
+		});
+	}
+
+	private Set<Method> methodsCalled(TournamentListener mock) {
+		return mockingDetails(mock).getInvocations().stream().map(Invocation::getMethod).collect(toSet());
 	}
 
 	private static Object[] emptyArgs(Method method) {
