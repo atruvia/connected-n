00e841f2f4ddde4f77d5cd92478b486fdee6374c
Peter Fichtner <Peter.Fichtner@fiduciagad.de>
2021-09-15 10:56:59 +0200

    javadoc added

--- core/src/main/java/org/ase/fourwins/season/Round.java
+++ core/src/main/java/org/ase/fourwins/season/Round.java
@@ -15,28 +15,55 @@ import lombok.Data;
 @Data
 public class Round<T> {
 
+	/**
+	 * When accessing this list at a certain index the result is not the element at
+	 * that index but the element next to it (better said n-elements, see
+	 * {@link #shiftBy}. This does <b>not</b> apply for the element at index
+	 * <code>0</code>.
+	 * <p>
+	 * So all elements but the first elements are rotated by {@link #shiftBy}.
+	 * <p>
+	 * Example: When accessing the underlying list with elements A,B,C,D via
+	 * {@link #get(int)} you will get the results as follow:
+	 * 
+	 * <pre>
+	 * given {@link #shiftBy} is <code>1</code>
+	 * index 0 -> index 0 (A) (element at index <code></code> is always fix)
+	 * index 1 -> index 2 (C)
+	 * index 2 -> index 3 (D)
+	 * index 3 -> index 1 (B)
+	 * 
+	 * given {@link #shiftBy} is <code>2</code>
+	 * index 0 -> index 0 (A) (element at index <code></code> is always fix)
+	 * index 1 -> index 3 (D)
+	 * index 2 -> index 1 (B)
+	 * index 3 -> index 2 (C)
+	 * </pre>
+	 *
+	 */
 	private static final class RotatedLeagueList<T> extends ListDelegate<T> {
 
-		private final int rotateBy;
+		/** how far should the shifting be done */
+		private final int shiftBy;
 
 		private RotatedLeagueList(List<T> delegate) {
 			this(delegate, 1);
 		}
 
-		private RotatedLeagueList(List<T> delegate, int rotateBy) {
+		private RotatedLeagueList(List<T> delegate, int shiftBy) {
 			// constructor is optimized by doing decomposition, this is pure performance
 			// optimization only, could be removed without changing behavior. Just done for fun (and no profit) :D
 			super(delegate instanceof RotatedLeagueList ? ((RotatedLeagueList<T>) delegate).getDelegate() : delegate);
-			this.rotateBy = delegate instanceof RotatedLeagueList ? ((RotatedLeagueList<T>) delegate).rotateBy + rotateBy : rotateBy;
+			this.shiftBy = delegate instanceof RotatedLeagueList ? ((RotatedLeagueList<T>) delegate).shiftBy + shiftBy : shiftBy;
 		}
 
 		@Override
 		public T get(int index) {
-			return super.get(index == 0 ? 0 : rotatedIndex(index));
+			return super.get(index == 0 ? 0 : shiftedIndex(index));
 		}
 
-		private int rotatedIndex(int index) {
-			return (rotateBy + index - 1) % (size() - 1) + 1;
+		private int shiftedIndex(int index) {
+			return (shiftBy + index - 1) % (size() - 1) + 1;
 		}
 
 	}
