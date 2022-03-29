42b523d075ce2987fb1cb6cc473a4029a8f0a3ef
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-03-29 22:35:45 +0200

    migrated to mvn wrapper

--- build.sh
+++ build.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 
-DOCKER_IMAGE=maven:3.6-jdk-11
-PRJ_NAME=`basename "$PWD"`
-
-docker run -it --rm \
--v "$HOME/.m2":/root/.m2 \
--v "$PWD":/usr/src/$PRJ_NAME \
--w /usr/src/$PRJ_NAME \
-$DOCKER_IMAGE \
-mvn package jib:buildTar && docker image load -i udp/target/jib-image.tar
+./mvnw package jib:buildTar && docker image load -i udp/target/jib-image.tar
 
